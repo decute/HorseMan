d84b624362b772756173da39d26132637429ac16
Ratan Panneerselvam <ratan.panneerselvam@olympus-ossa.com>
2020-10-07 15:06:42 -0400

    typo

--- src/android/HorseMan.java
+++ src/android/HorseMan.java
@@ -395,7 +395,7 @@ public class HorseMan extends CordovaPlugin {
                                 mG1_thicknessMin = getFloat(data, g1_idx+4)*velocity*((float) 0.5);
                                 //mG1_thicknessMax = getFloat(data, g2_idx+4)*velocity*((float) 0.5);
                                 //mG1_thickness = mG1_thicknessMax - mG1_thicknessMin;
-                                mG1_thicknessMax = 0.0;
+                                mG1_thicknessMax = 0;
                                 mG1_thickness = mG1_thicknessMin;
                                 mG2_len = getInt(data, g2_idx)/4;
                                 int ascan_pairs = getInt(data, 1+(1+mG2_len)+(1+mG2_len));
